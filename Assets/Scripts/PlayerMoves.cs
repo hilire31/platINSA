ba55ff5de0244f08385efb7aaf71bba56f863173
ZoubiFree <zegharisaad@gmail.com>
2024-10-04 20:08:24 +0200

    Le perso i bouj et i vol

--- Assets/Scripts/PlayerMoves.cs
+++ Assets/Scripts/PlayerMoves.cs
@@ -2,11 +2,17 @@ using UnityEngine;
 
 public class PlayerMoves : MonoBehaviour
 {
+    [SerializeField] private float speed;
     private Rigidbody2D body;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
     }
-
+    private void Update()
+    {
+        body.velocity = new Vector2(Input.GetAxis("Horizontal")*speed,body.velocity.y);
+    if (Input.GetKey(KeyCode.Space))
+        body.velocity = new Vector2(body.velocity.x, speed);
+    }
 }
