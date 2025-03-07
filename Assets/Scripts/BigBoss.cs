97c5af286071b8662e29704bddc462676152807f
hilire31 <firminrousseau@gmail.com>
2025-03-07 23:20:01 +0100

    scene management, boss level

--- Assets/Scripts/BigBoss.cs
+++ Assets/Scripts/BigBoss.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class BigBoss : MonoBehaviour
 {
     [SerializeField] private Transform endPoint;
     [SerializeField] private Transform speedPoint;
+    [SerializeField] private Transform slowPoint;
     [SerializeField] private float speed = 5f;
     
     [SerializeField] private float boostSpeed = 8f;
@@ -14,13 +16,19 @@ public class BigBoss : MonoBehaviour
     private bool isStopped=false;
     private int localDir;
     private Vector3 target;
+    private Vector3 originTransform;
+    private float speedInit,normalSpeed;
+    public float slowedDuration,slowedSpeed;
+    private bool isSlowed = true;
 
     private void Start()
     {
+        speedInit=speed;
         target = new Vector3(endPoint.position.x, transform.position.y, transform.position.z);
         localDir = (endPoint.position.x > transform.position.x) ? 1 : -1;
         transform.localScale = new Vector3(localDir * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
-        
+        originTransform = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        normalSpeed=speedInit;
     }
 
 
@@ -35,6 +43,9 @@ public class BigBoss : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision){
         if (collision.CompareTag("Player")){
             //SceneController.instance.NextLevel(); load longer level ?
+            transform.position=originTransform;
+            speed = speedInit;
+            
         }
     }
     private void MoveBetweenPoints()
@@ -48,9 +59,28 @@ public class BigBoss : MonoBehaviour
         if (speedPoint!=null){
             if (Mathf.Abs(transform.position.x - speedPoint.position.x) < 0.1f){
                 speed = boostSpeed;
+                normalSpeed=speed;
+            }
+
+        }
+        if (slowPoint!=null){
+            if (Mathf.Abs(transform.position.x - slowPoint.position.x) < 0.1f){
+                if (isSlowed){
+                    StartCoroutine(SlowSpeed());
+                }
             }
 
         }
         
+        
+    }
+    private IEnumerator SlowSpeed(){
+        
+        isSlowed=true;
+        speed = slowedSpeed;
+        
+        yield return new WaitForSeconds(slowedDuration);
+        
+        speed = normalSpeed;
     }
 }
--- Assets/Scripts/EndFlag.cs
+++ Assets/Scripts/EndFlag.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class EndFlag : MonoBehaviour
 {
     
-
+    [SerializeField] private SceneAsset scene; 
     private void OnTriggerEnter2D(Collider2D collision){
         if (collision.CompareTag("Player")){
-            SceneController.instance.NextLevel(); 
+            SceneController.instance.LoadScene(scene.name); 
         }
     }
 
